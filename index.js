fcdd4690cac1c1bb5f1397a59180ecc9c1f91d6d
howsilence <statton0104@gmail.com>
2022-01-11 10:24:15 -0500

    commit tree a71ba02de55eeb2adfaef16df87a324e144a2a04

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(arg = "roller-skate"){
+    return `This Saturday, I want to ${arg}!`
+};
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+};
+    function wrapAdjective(balls = "*"){
+        return function(param = "special"){
+            return `You are ${balls}${param}${balls}!`
+        };
+    }
